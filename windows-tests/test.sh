7cc8df6ff729e1caef0979658c2a1785a0d9e9bd
yxurrbzallkd <kypybida@ucu.edu.ua>
2022-12-29 21:38:12 +0200

    fixed windows tests

--- windows-tests/test.sh
+++ windows-tests/test.sh
@@ -1,56 +1,59 @@
-#/bin/bash
+#/usr/hello/bash
 option=$1 # if clean - delete exes
 
-mkdir bin
-cd bin
-cmake .. -G"Unix Makefiles"
-make
-cd ..
+#mkdir hello
+#cd hello
+#cmake .. -G"Unix Makefiles"
+#make
+#cd ..
 
 echo Running tests:
 
 echo doing hello
 ./hello.exe
+echo
 
 echo "doing echo hello from cmd"
-./basic_process_test.exe cmd.exe /c echo hello
+./basic_process_test.exe "cmd.exe" "\\/c" "echo" hello
 echo
 
-echo doing echo hello from powershell
-./basic_process_test.exe powershell.exe /c echo hello
+echo "doing echo hello from powershell"
+./basic_process_test.exe "powershell.exe" "echo" hello
 echo
 
-echo doing dir 
-./basic_process_test.exe cmd.exe /c dir
+mkdir hello
+echo hello > ./hello/file.txt
+./conveyor_test.exe
 echo
 
-echo doing ls
-./basic_process_test.exe powershell.exe /c dir
+#!! doesn't work
+echo "doing dir hello" 
+./basic_process_test.exe "cmd.exe" "\\/c" "dir" "hello"
+echo
+
+echo doing ls hello
+./basic_process_test.exe "powershell.exe" "ls" "hello"
 echo
 
 echo hello > file.txt
 echo doing cat a file with hello
-./basic_process_test.exe powershell.exe /c cat file.txt
+./basic_process_test.exe "powershell.exe" "cat" "file.txt"
 rm file.txt
-echo
-
-mkdir bin
-echo hello > ./bin/file.txt
-./conveyor_test.exe
-rm -r bin
 echo
 
 echo Basic shm test - expect to see read hello world and wrote hello world:
 ./shm_basic_test.exe
 echo
 
-echo Complicated shm test expect hello - broken!:
+echo Complicated shm test expect hello:
 ./shm_share_test.exe 0
 echo
 
 echo Basic fifo test hello world:
 ./fifo_test.exe hello
 echo
+
+rm -r hello
 
 if [[ option == "clean" ]]
 then
@@ -62,4 +65,3 @@ rm conveyor_test.exe
 rm hello.exe
 rm fifo_test.exe
 fi
-
